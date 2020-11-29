467b0a0a18ad631c89308a91096db3b30e5dfaed
Lucas lopes <lucasomena@lccv.ufal.br>
2020-11-29 09:03:40 -0300

    Update install_frankmocap.sh

--- scripts/install_frankmocap.sh
+++ scripts/install_frankmocap.sh
@@ -10,11 +10,3 @@ echo ""
 echo ">>  Installing a third-party hand detector"
 sh scripts/install_hand_detectors.sh
 
-echo ""
-if [ ! -d "sample_data" ] 
-then
-    echo "Downloading sample videos"
-    wget https://dl.fbaipublicfiles.com/eft/sampledata_frank.tar && tar -xvf sampledata_frank.tar && rm sampledata_frank.tar && mv sampledata sample_data
-else
-    echo "There exists sample_data already"
-fi
